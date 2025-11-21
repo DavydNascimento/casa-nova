9c533f23820646728f1c7c5f973f99e586da8d32
Davyd Nascimento <davyd.seth19@gmail.com>
2025-11-20 23:53:44 -0300

    atualizado

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Seleciona o botão "Presentear" pelo ID
-const btnPresentear = document.getElementById('btnPresentear');
-
-// Adiciona um evento de clique ao botão
-btnPresentear.addEventListener('click', () => {
-    // Redireciona para a página "presentear.html"
-    window.location.href = 'presentear.html';
-});
