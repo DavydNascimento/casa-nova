352c28a3daf0ca50abaf3a09b92a137951d4b87c
Davyd Nascimento <davyd.seth19@gmail.com>
2025-11-20 22:48:10 -0300

    chá de casa nova

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Seleciona o botão "Presentear" pelo ID
+const btnPresentear = document.getElementById('btnPresentear');
+
+// Adiciona um evento de clique ao botão
+btnPresentear.addEventListener('click', () => {
+    // Redireciona para a página "presentear.html"
+    window.location.href = 'presentear.html';
+});
